9c7784f0a19d9c8eaff225bc4c482c54a5969fd9
MedlockJ <70552237+MedlockJ@users.noreply.github.com>
2020-09-18 21:46:08 -0400

    Finished Version

--- week4/components/Sidebar.js
+++ week4/components/Sidebar.js
@@ -12,13 +12,14 @@ class Sidebar extends Component {
 
     this.props.menu.forEach( (item) => {
       // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
-		responseHTML = '<li>' + this.props.menu.item + '</li>';
+    //responseHTML = '<li>' + this.props.menu.item + '</li>';
+     responseHTML = responseHTML + '<li>'+item+'</li>';
 		
 	  //Personal Check
-		console.log(responseHTML)
+		//console.log(responseHTML)
     });
 	
-    return '<ul>' + responseHTML + '</ul>';
+    return responseHTML;
   }
 }
 
