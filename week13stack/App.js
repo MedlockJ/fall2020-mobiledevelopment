89a8ecdef8c07733ca1fd5155636110b8c6a0ef4
MedlockJ <70552237+MedlockJ@users.noreply.github.com>
2020-11-16 16:27:49 -0500

    Working Stack

--- week13stack/App.js
+++ week13stack/App.js
@@ -1,26 +1,60 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen(props) {
+function Home(props) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Button
-        title="Settings"
-        onPress={() => props.navigation.navigate('Settings')}
+        title="Sit Up Tracking"
+        onPress={() => props.navigation.navigate('Sit Ups')}
+      />
+      <Button
+        title="Push Up Tracking"
+        onPress={() => props.navigation.navigate('Push Ups')}
       />
     </View>
   );
 }
 
-function SettingsScreen(props) {
+function Sit(props) {
+  const [sit, changeSit] = useState(0);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Sit Ups: {sit}</Text>
+      <Button 
+        title="Increse Rep"
+        onPress={() => changeSit(sit+1)} 
+      />
+      <Button
+        title="Reset"
+        onPress={() => changeSit(sit*0)}
+      />
+      <Button
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function Push(props) {
+  const [push, changePush] = useState(0);
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Push Ups: {push}</Text>
+      <Button
+        title="Change Rep"
+        onPress ={() => changePush(push+1)}
+      />
+      <Button
+        title="Reset"
+        onPress={() => changePush(push*0)}
+      />
       <Button
         title="Back to Home"
         onPress={() => props.navigation.navigate('Home')}
@@ -35,8 +69,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Sit Ups" component={Sit} />
+        <Stack.Screen name="Push Ups" component={Push} />
       </Stack.Navigator>
     </NavigationContainer>
   );
