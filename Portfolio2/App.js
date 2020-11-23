e8d50b56cae97425c8b0914baaef7961145dbfc1
MedlockJ <70552237+MedlockJ@users.noreply.github.com>
2020-11-23 17:40:40 -0500

    Operational Update

--- Portfolio2/App.js
+++ Portfolio2/App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -10,63 +10,136 @@ import { createStackNavigator } from '@react-navigation/stack';
 //Food Menu App
 
 function Home(props) {
-  const [push, changePush] = useState(0);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Push Ups: {push}</Text>
+      <Text>Welcome to Generic Restaraunt!</Text>
       <Button
-        title="Menu Item"
-        onPress ={() => changePush(push+1)}
+        title="Brunch"
+        onPress ={() => props.navigation.navigate('Brunch')}
       />
       <Button
-        title="Menu Item"
-        onPress={() => changePush(push*0)}
+        title="Lunch"
+        onPress={() => props.navigation.navigate('Lunch')}
       />
       <Button
-        title="Back to Home"
-        onPress={() => props.navigation.navigate('Home')}
+        title="Drinks"
+        onPress={() => props.navigation.navigate('Drinks')}
       />
     </View>
   );
 }
 
 function Brunch(props) {
-  const [push, changePush] = useState(0);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Push Ups: {push}</Text>
+      <Text>Brunch Specials</Text>
       <Button
-        title="Menu Item"
-        onPress ={() => changePush(push+1)}
+        title="Details"
+        onPress ={() => props.navigation.navigate('BrunchItem')}
       />
       <Button
-        title="Menu Item"
-        onPress={() => changePush(push*0)}
+        title="Details"
+        onPress={() => props.navigation.navigate('BrunchItem2')}
       />
       <Button
-        title="Back to Home"
+        title="Return Home"
         onPress={() => props.navigation.navigate('Home')}
       />
     </View>
   );
 }
 
-function Brunch(props) {
+function Lunch(props) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Lunch Menu 11AM-5PM</Text>
+      <Button
+        title="Detais"
+        onPress ={() => props.navigation.navigate('LunchItem')}
+      />
+      <Button
+        title="Detaila"
+        onPress={() => props.navigation.navigate('LunchItem2')}
+      />
+      <Button
+        title="Return Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function Drinks(props) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Drinks Available on Tap</Text>
+      <Button
+        title="Detais"
+        onPress ={() => props.navigation.navigate('DrinkItem')}
+      />
+      <Button
+        title="Details"
+        onPress={() => props.navigation.navigate('DrinkItem2')}
+      />
+      <Button
+        title="Return Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function BrunchItem(props) {
   const [push, changePush] = useState(0);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Push Ups: {push}</Text>
+      <Text>Chicken and Waffles</Text>
+
       <Button
-        title="Menu Item"
-        onPress ={() => changePush(push+1)}
+        title="Back"
+        onPress={() => props.navigation.navigate('Brunch')}
       />
+    </View>
+  );
+}
+
+function BrunchItem2(props) {
+  const [push, changePush] = useState(0);
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Biscuits and Gravy</Text>
+
       <Button
-        title="Menu Item"
-        onPress={() => changePush(push*0)}
+        title="Back"
+        onPress={() => props.navigation.navigate('Brunch')}
       />
+    </View>
+  );
+}
+
+function LunchItem(props) {
+  const [push, changePush] = useState(0);
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Buffalo Mac</Text>
+
       <Button
-        title="Back to Home"
-        onPress={() => props.navigation.navigate('Home')}
+        title="Back"
+        onPress={() => props.navigation.navigate('Lunch')}
+      />
+    </View>
+  );
+}
+
+function LunchItem2(props) {
+  const [push, changePush] = useState(0);
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Burger</Text>
+
+      <Button
+        title="Back"
+        onPress={() => props.navigation.navigate('Lunch')}
       />
     </View>
   );
@@ -75,13 +148,26 @@ function Brunch(props) {
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
+function Root() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Brunch" component={Brunch} />
+      <Stack.Screen name="Lunch" component={Lunch} />
+      <Stack.Screen name="Drinks" component={Drinks} />
+      <Stack.Screen name="BrunchItem" component={BrunchItem} />
+      <Stack.Screen name="BrunchItem2" component={BrunchItem2} />
+      <Stack.Screen name="LunchItem" component={LunchItem} />
+      <Stack.Screen name="LunchItem2" component={LunchItem2} />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Sit Ups" component={Sit} />
-        <Drawer.Screen name="Push Ups" component={Push} />
+        <Drawer.Screen name="Root" component={Root} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
